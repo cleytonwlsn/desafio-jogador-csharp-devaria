c4a1f2b27084ef1117b64d248b7bf68be6e3096f
cleytonwlsn <cleytonwilsonlima@gmail.com>
2023-11-25 16:03:29 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioJogador/Program.cs
@@ -0,0 +1,22 @@
+﻿string nomeJogador;
+
+Console.WriteLine("--Bem-vindos ao torneio de Poker Devaria!--");
+
+Console.WriteLine("Qual o nome do jogador?");
+
+nomeJogador = Console.ReadLine().ToString();
+
+switch (nomeJogador)
+{
+    case "Daniel":
+    case "Rafael":
+    case "Kaique":
+    case "Douglas":
+    case "Victor":
+    case "Fulano": 
+        Console.WriteLine("Jogador autorizado para o torneio de Poker!");
+        break;
+    default:
+        Console.WriteLine("Jogador não autorizado para o torneio de Poker!");
+        break;
+}
